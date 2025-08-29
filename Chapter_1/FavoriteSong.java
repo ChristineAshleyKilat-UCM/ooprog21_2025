a20f72be36a2a9163b4becc75c3c2b16ea74a4cd
ChristineAshleyKilat-UCM <liamsebastian567@gmail.com>
2025-08-29 17:54:15 +0800

    Add files via upload

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,10 @@
+public class FavoriteSong {
+   public static void main(String[] args) {
+   
+   System.out.println("I poured my heart in it");
+   System.out.println("Don't get me started ");
+   System.out.println("Spend my time wondering why");
+   System.out.println("I spend my 20's on you");
+   
+   }
+}
