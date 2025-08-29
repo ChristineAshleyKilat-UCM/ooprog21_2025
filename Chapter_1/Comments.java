58f084b0e60da7f97ef2eb4b9264cb3fadb7fcbb
ChristineAshleyKilat-UCM <liamsebastian567@gmail.com>
2025-08-29 17:55:16 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,25 @@
+/**
+   This is a documentation comment 
+   This program contains the 3-different types of comments in java
+   1. Single-line comment
+   2. Multi-line comment
+   3. Documentation comment
+*/
+public class Comments {
+   public static void main(String[] args) {
+   
+   // This is a single-line comment
+   System.out.println("A single-line comment");
+   /*
+    This is a multi-line comment
+    Useful when your classmate will look at your code
+    It can save you time to explain it to them
+    Be careful though because some of them will copy your code*/
+   System.out.println("A multi-line comment");
+   
+   }
+}
+    
+  
+    
+    
