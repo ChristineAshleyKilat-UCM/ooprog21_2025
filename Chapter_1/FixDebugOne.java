ff8a8cc21e5158fba41d37a1ff7bc99c2e104f85
ChristineAshleyKilat-UCM <liamsebastian567@gmail.com>
2025-09-01 20:55:19 +0800

    Add files via upload

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,25 @@
+//Bugs Version
+/*public class FixDebug One
+{ */
+/* This program displays a greeting */
+/*public static void Main(String args)
+{
+Systm.out.println("Hello World!")
+} */
+/*
+Bugs:
+1. The class name should be FixDebugOne, there shouldn't be a space
+2. The Main should be in lowercase
+3. The method should be (String[] args)
+4. Systm.out.println("Hello World!"), it should be like this System.out.println("Hello World!");
+5. Missing a closing curly braket 
+*/
+
+//Fixed Version
+public class FixDebugOne {
+   public static void main(String[] args) {
+   
+   System.out.println("Hello World!");
+   
+   }
+}
